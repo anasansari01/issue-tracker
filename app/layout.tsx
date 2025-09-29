e07758f83df9e0d019ef6dd4945fc5555ee4ba5c
Anas Ansari <anasansari056789@gmail.com>
2025-09-29 11:29:56 +0530

    Add the edit button

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           <main className='p-5'>
             {children}
           </main>
-          <ThemePanel/>
+          {/* <ThemePanel/> */}
         </Theme>
       </body>
     </html>
