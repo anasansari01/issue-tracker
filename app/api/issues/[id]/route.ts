53920c93cfadd751345719a111f59acadf4b9700
Anas Ansari <anasansari056789@gmail.com>
2025-10-01 00:13:53 +0530

    Handle errors

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -3,38 +3,58 @@ import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(request: NextRequest, {params}: {params : {id: string}}){
-  const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  try{
+    const body = await request.json();
+    const validation = issueSchema.safeParse(body);
 
-  if(!validation.success) return NextResponse.json(validation.error.format, {status: 400});
+    if(!validation.success) return NextResponse.json(validation.error.format, {status: 400});
 
-  const {id} = await params;
-  const issue = await prisma.issue.findUnique({
-    where: {id: parseInt(id)}
-  });
+    const {id} = await params;
+    const issue = await prisma.issue.findUnique({
+      where: {id: parseInt(id)}
+    });
 
-  if(!issue) return NextResponse.json({error: 'invalid Issue'}, {status: 400});
+    if(!issue) return NextResponse.json({error: 'Failed to Update issue'}, {status: 400});
 
-  const updateIssue = await prisma.issue.update({
-    where: {id: issue.id},
-    data:{
-      title: body.title,
-      description: body.description
-    }
-  })
-  return NextResponse.json(updateIssue, {status: 201});
+    const updateIssue = await prisma.issue.update({
+      where: {id: issue.id},
+      data:{
+        title: body.title,
+        description: body.description
+      }
+    })
+    return NextResponse.json(updateIssue, {status: 201});
+  }catch(error){
+    return NextResponse.json(
+      { error: "Invalid Issue" },
+      { status: 500 }
+    );
+  }
 }
 
-export async function DELETE(request: NextRequest, {params}: {params : {id: string}}){
-  const {id} = await params;
-  const issue = await prisma.issue.findUnique({
-    where: {id: parseInt(id)}
-  });
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = params;
 
-  if(!issue) return NextResponse.json({error: 'invalid Issue'}, {status: 400});
+    const issue = await prisma.issue.findUnique({
+      where: { id: parseInt(id) },
+    });
 
-  await prisma.issue.delete({
-    where: {id: issue.id}
-  })
-  return NextResponse.json({ message: 'Issue deleted successfully' }, {status: 204});
+    if (!issue)
+      return NextResponse.json({ error: "Failed to delete issue" }, { status: 404 });
+
+    await prisma.issue.delete({
+      where: { id: issue.id },
+    });
+
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Invalid Issue" },
+      { status: 500 }
+    );
+  }
 }
