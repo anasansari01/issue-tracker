00c77e0be44b6a08e8dbbc0b1685edda1ddf7987
Anas Ansari <anasansari056789@gmail.com>
2025-10-01 21:56:10 +0530

    Remove duplicate skeletons

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Link = ({ href, children}: Props) => {
   return (
-    <NextLink href={href} passHref>
+    <NextLink href={href} passHref legacyBehavior>
       <RadixLink>{children}</RadixLink>
     </NextLink>
   )
