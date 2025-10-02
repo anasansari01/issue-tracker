1ef24a872c00f246a64ce532509ede2f53855e3c
Anas Ansari <anasansari056789@gmail.com>
2025-10-02 22:25:59 +0530

    Paginate issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -5,9 +5,14 @@ import NextLink from "next/link";
 import IssueActions from "./IssueActions";
 import { Issue, Status } from "@prisma/client";
 import { ArrowUpIcon } from "@radix-ui/react-icons";
+import Pagination from "@/app/components/Pagination";
 
 interface Props {
-  searchParams: { status: Status; orderBy: keyof Issue };
+  searchParams: { 
+    status: Status,
+    orderBy: keyof Issue,
+    page: string,
+  };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
@@ -26,24 +31,26 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const status = statuses.includes(resolveParams.status)
     ? resolveParams.status
     : undefined;
-  
-  console.log(searchParams);
 
+  const where = {status};
+  
   const orderBy = columns
     .map(column => column.value)
     .includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: 'asc' }
     : undefined;
 
+  const page = parseInt(searchParams.page) || 1;
+  const pageSize = 10;
+
   const issues = await prisma.issue.findMany({
     where: { status },
-    orderBy
+    orderBy,
+    skip: (page - 1) * pageSize,
+    take: pageSize
   });
 
-  console.log(orderBy);
-  console.log(status);
-  console.log(statuses);
-  console.log(issues);
+  const issueCount = await prisma.issue.count({ where })
 
   return (
     <div>
@@ -89,6 +96,11 @@ const IssuesPage = async ({ searchParams }: Props) => {
           ))}
         </Table.Body>
       </Table.Root>
+      <Pagination
+        pageSize={pageSize}
+        currentPage={page}
+        itemCount={issueCount}
+      />
     </div>
   );
 };
